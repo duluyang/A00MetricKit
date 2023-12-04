d5fcfa2efc205912a13696527f89d4dd6bd4e74c
denglibing5 <denglibing3@jd.com>
2023-12-04 17:36:13 +0800

    添加通过 xctest 执行获取启动耗时代码

--- A00MetricKitUITests/A00MetricKitUITestsLaunchTests.swift
+++ A00MetricKitUITests/A00MetricKitUITestsLaunchTests.swift
@@ -8,24 +8,14 @@
 import XCTest
 
 final class A00MetricKitUITestsLaunchTests: XCTestCase {
-
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
     func testLaunch() throws {
         let bundleid = ProcessInfo.processInfo.environment["BUNDLE_ID"]!
+//        let jxtj = "com.jd.jdmobilelite"
+//        let jx = "com.360buy.jdpingou"
         print("bundleid: \(bundleid)")
         let options = XCTMeasureOptions()
         options.iterationCount = 1
-        measure(metrics: [XCTOSSignpostMetric.applicationLaunch], options: options) {
-            // Clean up the application state before each launch, if necessary
-//                let jx = "com.360buy.jdpingou"
-//                let jxtj = "com.jd.jdmobilelite"
+        measure(metrics: [XCTApplicationLaunchMetric()], options: options) {
             let app = XCUIApplication(bundleIdentifier: bundleid)
             app.launch()
         }
--- xctest.sh
+++ xctest.sh
@@ -1,17 +1,44 @@
 cdate=$(date "+%Y%m%d%H%M%S")
+
 # 测试手机的 udid
-udid="00008101-001645DA2690001E"
-#udid="00008110-001A31C6362A801E"
+# udid="00008101-001645DA2690001E"
+# udid="00008110-001A31C6362A801E"
+udid="00008020-000A2C1A0282002E"
+
 # 京喜特价的包名
 #bundleid="com.jd.jdmobilelite"
 # 京东的包名
 bundleid="com.360buy.jdmobile"
 
-xcodebuild clean
+# 当前生成的数据目录
+cpath="xcresult/${bundleid}/${cdate}"
+# xcodebuild test 生成的 xcresult 数据
+resultBundlePath="${cpath}/${bundleid}_${cdate}.xcresult"
 
-xcodebuild test \
--project A00MetricKit.xcodeproj \
--scheme A00MetricKitUITests \
--destination "platform=iOS,id=${udid}" \
--resultBundlePath xcresult/${bundleid}_${cdate}.xcresult \
-BUNDLE_ID=${bundleid} \
+# 解析 xcresult 二进制文件后的数据
+logpath="${cpath}/logs"
+
+xcodebuildTest() {
+	xcodebuild clean
+
+	xcodebuild test \
+	-project A00MetricKit.xcodeproj \
+	-scheme A00MetricKitUITests \
+	-destination "platform=iOS,id=${udid}" \
+	-resultBundlePath ${resultBundlePath} \
+	BUNDLE_ID=${bundleid}
+}
+
+analysisXcresult() {
+	ruby ruby/unitTestLog.rb \
+	--xcresult-path=${resultBundlePath} \
+	--output-path=${logpath}
+}
+
+
+xcodebuildTest
+if [ $? -eq 0 ]; then
+	analysisXcresult
+else
+	echo "xcodebuild test 失败"
+fi
